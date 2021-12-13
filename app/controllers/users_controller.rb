44ed15a519368cac2d74f59a1b96ad2f5cd45a09
Pavel <pavel@wearepush.co>
2021-12-13 19:42:06 +0200

    task 51-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,57 +1,83 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
-  WORD_VERSION = %w[вопрос вопроса вопросов]
+  QUESTION_VERSION = %w[вопрос вопроса вопросов]
+  ANSWER_VERSION = %w[ответ ответа ответов]
+  UNANSWERED_VERSION = %w[безответный безответных безответных]
+
+  before_action :load_user, except: [:index, :create, :new]
+
+  before_action :authorize_user, except: [:index, :new, :create, :show]
 
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Vadim',
-        username: 'installero',
-        avatar_url: 'https://secure.gravatar.com/avatar/' \
-          '71269686e0f757ddb4f73614f43ae445?s=100'
-      ),
-      User.new(id: 2, name: 'Misha', username: 'aristofun')
-    ]
+    @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Пользователь успешно зарегестрирован!'
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @user = User.new(
-      name: 'Vadim',
-      username: 'installero',
-      avatar_url: 'https://secure.gravatar.com/avatar/' \
-        '71269686e0f757ddb4f73614f43ae445?s=100'
-    )
+    @questions = @user.questions.order(created_at: :desc)
 
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-      Question.new(
-        text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')
-      )
-    ]
+    @new_question = @user.questions.build
 
-    @right_version_word = right_version_word(@questions.count)
+    questions_count = @questions.count.presence || 0
+    answers_count = @questions.where.not(answer: nil).count.presence || 0
+    unanswered_count = questions_count - answers_count
 
-    @new_question = Question.new
-
+    @questions_count_text = fetch_right_version_text(questions_count, QUESTION_VERSION)
+    @answers_count_text = fetch_right_version_text(answers_count, ANSWER_VERSION)
+    @unanswered_count_text = fetch_right_version_text(unanswered_count, UNANSWERED_VERSION)
   end
 
   private
 
-  def right_version_word(questions_count)
-    return WORD_VERSION[2] if (11..14).include?(questions_count % 100)
+  def authorize_user
+    reject_user unless @user == current_user
+  end
 
-    case questions_count % 10
-    when 1 then WORD_VERSION[0]
-    when 2..4 then WORD_VERSION[1]
-    else WORD_VERSION[2]
-    end
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :name, :username, :avatar_url)
+  end
+
+  def fetch_right_version_text(item_count, word_array)
+    return word_array[2] if (11..14).include?(item_count % 100)
+
+    right_version_word = case item_count % 10
+                          when 1 then word_array[0]
+                          when 2..4 then word_array[1]
+                          else word_array[2]
+                          end
+
+    "<h1 class'title-small'>#{item_count}</h1><p>#{right_version_word}</p>"
   end
 end
