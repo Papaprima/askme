fd7f5e5ea1bb8e1e80930bb0e67142de0d941607
Pavel <pavel@wearepush.co>
2021-12-14 11:22:23 +0200

    fix migration

--- db/migrate/20211213182236_add_autor_to_questions.rb
+++ db/migrate/20211213182236_add_autor_to_questions.rb
@@ -2,6 +2,6 @@ class AddAutorToQuestions < ActiveRecord::Migration[5.2]
   def change
     add_reference :questions, :author, index: true
 
-    add_foreign_key :questions, :user, column: :author_id
+    add_foreign_key :questions, :users, column: :author_id
   end
 end
